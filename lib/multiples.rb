f4e59c608aa1381d87bb8e497de6bc8da48e1fd0
makafis <vinnyalfieri@gmail.com>
2015-07-01 09:46:00 -0400

    complete

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,28 @@
-# Enter your procedural solution here!
+def sum_multiples(limit)
+number_array = []
+current_num = 1
+    while current_num < limit
+      if current_num % 3 == 0
+        number_array << current_num
+      elsif current_num % 5 == 0
+        number_array << current_num
+      end
+      current_num += 1
+    end #end while
+number_array.inject{|sum,x| sum + x}
+end
+
+
+def collect_multiples(limit)
+  number_array = []
+  current_num = 1
+    while current_num < limit
+      if current_num % 3 == 0
+        number_array << current_num
+      elsif current_num % 5 == 0
+        number_array << current_num
+      end
+      current_num += 1
+    end #end while
+  number_array
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,37 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+      number_array = []
+  current_num = 1
+    while current_num < @limit
+      if current_num % 3 == 0
+        number_array << current_num
+      elsif current_num % 5 == 0
+        number_array << current_num
+      end
+      current_num += 1
+    end #end while
+  number_array
+  end
+
+  def sum_multiples
+    number_array = []
+    current_num = 1
+        while current_num < @limit
+          if current_num % 3 == 0
+            number_array << current_num
+          elsif current_num % 5 == 0
+            number_array << current_num
+          end
+          current_num += 1
+        end #end while
+    number_array.inject{|sum,x| sum + x}
+  end
+
+
+end
